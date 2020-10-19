8e3e95489b011c48e2099ba7f67822dac2433ff4
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2020-10-19 20:04:25 -0300

    feat(base): first commit with configuraition and first version

---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const app = express();
+
+let partidas = [];
+let verificadorSimples = {};
+app.use(express.json());
+
+app.get('/', function(req, res){
+    res.send({
+        message: "Bem vindo ao jogo da velha!",
+        next: "GET /partida/nova"
+    })
+})
+
+app.get('/partida/nova', function(req, res){
+    const verificationCode = Math.round(Math.random()*100);
+    const novaPartida = partidas.push({
+        player1: {},
+        player2: {},
+        winner: 0,
+        verificationCode
+    });
+
+    res.send({
+        message: "Foi criada uma nova partida",
+        // next: "Use o código de verificação para poder enviar jogadas",
+        idPartida: novaPartida - 1,
+        // code: verificationCode
+    })
+});
+
+app.post('/partida/:partida/jogar/:user', function(req,res){
+    const partida = req.params.partida;
+    // const code = req.body.code;
+    // if(code !== partidas[partida].verificationCode){
+    //     res.send({
+    //         "message": "Código de verificação inválido"
+    //     });
+
+    //     return false;
+    // }
+
+    const user = req.params.user;
+
+    if(
+        (user !== 'player1') ||
+        (user !== 'player2')
+    ) {
+        res.send({
+            "message": "Usuário inválido"
+        });
+
+        return false;
+    }
+
+    const jogada = req.body.jogada;
+
+    if(
+        (jogada !== 11) ||
+        (jogada !== 12) ||
+        (jogada !== 13) ||
+        (jogada !== 21) ||
+        (jogada !== 22) ||
+        (jogada !== 23) ||
+        (jogada !== 31) ||
+        (jogada !== 32) ||
+        (jogada !== 33) ||
+        (partida[user].includes(jogada))
+    ) {
+        res.send({
+            "message": "Jogada Inválida",
+            "error": (jogada in partida[user]) ? "Jogada indisponível" : "Jogdada inexistente"
+        });
+
+        return false;
+    }
+    
+    if(
+        (partida[user].includes([11,12,13])) ||
+        (partida[user].includes([11,22,33])) ||
+        (partida[user].includes([11,21,31])) ||
+        (partida[user].includes([13,23,33])) ||
+        (partida[user].includes([31,32,33]))
+    ) {
+        partida[winner] = jogador;
+        
+        res.send({
+            "message": "Jogada Ganhadora!",
+            "congratulations": "Parabéns, você ganhou."
+        });
+
+        return false;
+    }
+});
+
+app.listen(3000, function(){
+    console.log("Server listening at localhost:3000")
+});
