6170d5475bbd20cd20b2988c47f75c0523966bc5
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2020-10-21 20:41:25 -0300

    fix(server): correção em partidas

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/partida/nova', function(req, res){
 app.get('/partida/:partida/status/:user', function(req,res){
     const partida = req.params.partida;
 
-    if(!isNaN(partida) || (partida < 0) || !todasPartidas.length < partida){
+    if(isNaN(partida) || (partida < 0) || todasPartidas.length < partida){
         res.send({
             "message": "Partida inválida"
         });
